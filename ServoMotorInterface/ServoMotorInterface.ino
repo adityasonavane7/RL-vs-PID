4694d04e778a49e69c971e4fcfa0323408ddc852
adityasonavane7 <adityasonavane7@gmail.com>
2019-04-04 22:43:15 +0530

    Servo motor interface to Serial

--- ServoMotorInterface/ServoMotorInterface.ino
+++ ServoMotorInterface/ServoMotorInterface.ino
@@ -1,28 +1,19 @@
-// Sweep
-// by BARRAGAN <http://barraganstudio.com> 
-// This example code is in the public domain.
-
-
 #include <Servo.h> 
  
-Servo myservo;  // create servo object to control a servo 
-                // a maximum of eight servo objects can be created 
- 
+Servo myservo;  // create servo object to control a servo  
 int pos = 0;    // variable to store the servo position 
- int q;
+int q;
 void setup() 
 { 
-  q = 35;
   Serial.begin(115200);
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
 } 
- 
  
 void loop() 
 { 
   if (Serial.available() > 0){
     q = Serial.parseInt();
-    if (q >= 25 && q <= 34){ 
+    if (q >= 25 && q <= 34){ // Constrain servo between 25 deg and 34 deg 
       myservo.write(q);
     }else if (q <25){
       myservo.write(25);
